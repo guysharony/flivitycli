346eafdebf8825fdff6f43a9d50a5cc7733295a9
guysharony <sharonyguy@gmail.com>
2022-01-13 09:09:15 +0100

    Fixing compiler

--- build/libs/project/index.js
+++ build/libs/project/index.js
@@ -1,4 +1,23 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -9,12 +28,11 @@ const fs_extra_1 = __importDefault(require("fs-extra"));
 const yaml_1 = __importDefault(require("yaml"));
 const path_1 = __importDefault(require("path"));
 const serialize_javascript_1 = __importDefault(require("serialize-javascript"));
+const files = __importStar(require("../files"));
 const loadConfig = (dir) => {
     let data = null;
     try {
         data = require(path_1.default.join(dir, '.flv', 'index.js'));
-        data.input = path_1.default.join(dir, data.input);
-        data.output = path_1.default.join(dir, data.output);
     }
     catch (e) {
         return (null);
@@ -23,6 +41,14 @@ const loadConfig = (dir) => {
 };
 const load = (dir) => {
     const compiled = loadConfig(dir);
+    compiled.input = {
+        absolute: path_1.default.join(dir, compiled.input),
+        relative: compiled.input
+    };
+    compiled.output = {
+        absolute: path_1.default.join(dir, compiled.output),
+        relative: compiled.output
+    };
     const parseVariables = (vars = {}) => {
         const __base = (imported, prefix = null) => {
             let exported = {};
@@ -46,11 +72,6 @@ const load = (dir) => {
                 value = (!['string', 'number'].includes(typeof value)) ? (0, serialize_javascript_1.default)(value) : value;
                 data = data.replace(new RegExp(`%__${key.toUpperCase()}__%`, 'g'), value);
             }));
-            fs_1.default.mkdir(path_1.default.dirname(`${destination}.json`), { recursive: true }, function (err) {
-                if (err)
-                    return null;
-                fs_1.default.writeFileSync(`${destination}.json`, JSON.stringify(yaml_1.default.parse(data), null, '\t').replace(/: "(?:[^"]+|\\")*",?$/gm, ' $&'));
-            });
             /*
             fs.mkdir(path.dirname(destination), { recursive: true }, function (err) {
                 if (err) return null;
@@ -61,13 +82,76 @@ const load = (dir) => {
         },
         apply: async (vars = {}) => {
             const variables = parseVariables(vars);
-            for (const server in compiled.servers) {
+            for (const server_name in compiled.servers) {
+                const server = compiled.servers[server_name];
+                const compose_file = path_1.default.join(compiled.output.absolute, server_name, server.file);
+                const secrets_dir = {
+                    relative: path_1.default.join(server.secrets),
+                    absolute: path_1.default.join(compiled.output.absolute, server_name, server.secrets)
+                };
+                const imported = compiled.servers[server_name];
+                for (const service_name in imported.compose.services) {
+                    const service = imported.compose.services[service_name];
+                    const service_secrets = service.secrets;
+                    if ('build' in service) {
+                        const inputContext = path_1.default.join(compiled.input.absolute, server_name, service.build.context);
+                        const outputContext = path_1.default.join(compiled.output.absolute, server_name, service.build.context);
+                        await files.replaceVars(inputContext, outputContext, vars);
+                    }
+                    const service_secrets_absolute = secrets_dir.absolute.replace(new RegExp('%__service__%', 'g'), service_name);
+                    const service_secrets_relative = secrets_dir.relative.replace(new RegExp('%__service__%', 'g'), service_name);
+                    let secrets = [];
+                    for (const secret_name in service_secrets) {
+                        const compose_secret_name = `${service_name}__${secret_name}`.toLowerCase();
+                        const secret_absolute = path_1.default.join(service_secrets_absolute, secret_name);
+                        const secret_relative = path_1.default.join(service_secrets_relative, secret_name);
+                        const createSecret = async () => new Promise((resolve, rejects) => {
+                            fs_1.default.mkdir(path_1.default.dirname(secret_absolute), { recursive: true }, function (err) {
+                                if (err)
+                                    return rejects(null);
+                                const secret_value = service_secrets[secret_name];
+                                fs_1.default.writeFileSync(secret_absolute, (!['string', 'number'].includes(typeof secret_value)) ? (0, serialize_javascript_1.default)(secret_value) : `${service_secrets[secret_name]}`);
+                                if (!('secrets' in imported.compose))
+                                    compiled.servers[server_name].compose['secrets'] = {};
+                                compiled.servers[server_name].compose.secrets[compose_secret_name] = { file: secret_relative };
+                                secrets.push(compose_secret_name);
+                                return resolve(compose_secret_name);
+                            });
+                        });
+                        await createSecret();
+                    }
+                    if (secrets.length) {
+                        compiled.servers[server_name].compose.services[service_name].secrets = secrets;
+                    }
+                }
+                fs_1.default.mkdir(path_1.default.dirname(compose_file), { recursive: true }, function (err) {
+                    if (err)
+                        return null;
+                    fs_1.default.writeFileSync(compose_file, yaml_1.default.stringify(compiled.servers[server_name].compose));
+                });
+                /*
                 const imported = compiled.servers[server];
-                if (imported.file)
-                    imported.file = path_1.default.join(compiled.output, imported.file);
-                if (imported.secrets)
-                    imported.secrets = path_1.default.join(compiled.output, imported.secrets);
-                console.log(imported);
+
+                const file = path.join(compiled.output, imported.file);
+                const secrets = path.join(compiled.output, imported.secrets);
+
+                console.log(file, imported.file);
+                console.log(secrets, imported.secrets);
+
+                for (const service in imported.compose.services) {
+                    const secretsDir = imported.secrets.replace(new RegExp('%__service__%', 'g'), service);
+
+                    const secrets = imported.compose.services[service].secrets;
+
+                    for (const secret in secrets) {
+                        const secretsRel = secretsDir;
+                        const secretsAbs = path.join(outputAbsDir, secretsDir);
+
+                        console.log(path.join(secretsRel, secret), ' ==> ', path.join(secretsAbs, secret));
+                        // console.log(`${path.join(compiled.output, secretsDir)} == ${secret} => ${secrets[secret]}`);
+                    }
+                }
+                */
             }
             /*
             for (const compose in compiled.composes) {
--- src/libs/project/index.ts
+++ src/libs/project/index.ts
@@ -5,6 +5,8 @@ import path from 'path';
 import serialize from 'serialize-javascript';
 import * as compiler from './compiler';
 import * as files from '../files';
+import { rejects } from 'assert';
+import { resolve } from 'path/posix';
 
 
 interface Vars {
@@ -17,8 +19,6 @@ const loadConfig = (dir: string) => {
 
 	try {
 		data = require(path.join(dir, '.flv', 'index.js'));
-		data.input = path.join(dir, data.input);
-		data.output = path.join(dir, data.output);
 	} catch (e) {
 		return (null);
 	}
@@ -31,6 +31,14 @@ export type ProjectConfig = compiler.ProjectConfig;
 
 export const load = (dir: string) => {
 	const compiled = loadConfig(dir);
+	compiled.input = {
+		absolute: path.join(dir, compiled.input),
+		relative: compiled.input
+	};
+	compiled.output = {
+		absolute: path.join(dir, compiled.output),
+		relative: compiled.output
+	};
 
 	const parseVariables = (vars: Vars = {}) => {
 		const __base = (imported: Vars, prefix: string | null = null) => {
@@ -62,12 +70,6 @@ export const load = (dir: string) => {
 				data = data.replace(new RegExp(`%__${key.toUpperCase()}__%`, 'g'), value);
 			}));
 
-			fs.mkdir(path.dirname(`${destination}.json`), { recursive: true }, function (err) {
-				if (err) return null;
-
-				fs.writeFileSync(`${destination}.json`, JSON.stringify(YAML.parse(data), null, '\t').replace(/: "(?:[^"]+|\\")*",?$/gm, ' $&'));
-			});
-
 			/*
 			fs.mkdir(path.dirname(destination), { recursive: true }, function (err) {
 				if (err) return null;
@@ -79,13 +81,92 @@ export const load = (dir: string) => {
 		apply: async (vars: Vars = {}) => {
 			const variables = parseVariables(vars);
 
-			for (const server in compiled.servers) {
+			for (const server_name in compiled.servers) {
+				const server = compiled.servers[server_name];
+				const compose_file = path.join(compiled.output.absolute, server_name, server.file);
+				const secrets_dir = {
+					relative: path.join(server.secrets),
+					absolute: path.join(compiled.output.absolute, server_name, server.secrets)
+				};
+
+				const imported = compiled.servers[server_name];
+
+				for (const service_name in imported.compose.services) {
+					const service = imported.compose.services[service_name];
+					const service_secrets = service.secrets;
+
+					if ('build' in service) {
+						const inputContext = path.join(compiled.input.absolute, server_name, service.build.context);
+						const outputContext = path.join(compiled.output.absolute, server_name, service.build.context);
+
+						await files.replaceVars(inputContext, outputContext, vars);
+					}
+
+					const service_secrets_absolute = secrets_dir.absolute.replace(new RegExp('%__service__%', 'g'), service_name);
+					const service_secrets_relative = secrets_dir.relative.replace(new RegExp('%__service__%', 'g'), service_name);
+
+					let secrets: string[] = [];
+
+					for (const secret_name in service_secrets) {
+						const compose_secret_name = `${service_name}__${secret_name}`.toLowerCase();
+
+						const secret_absolute = path.join(service_secrets_absolute, secret_name);
+						const secret_relative = path.join(service_secrets_relative, secret_name);
+
+						const createSecret = async () => new Promise((resolve, rejects) => {
+							fs.mkdir(path.dirname(secret_absolute), { recursive: true }, function (err) {
+								if (err) return rejects(null);
+
+								const secret_value = service_secrets[secret_name];
+
+								fs.writeFileSync(secret_absolute, (!['string', 'number'].includes(typeof secret_value)) ? serialize(secret_value) : `${service_secrets[secret_name]}`);
+
+								if (!('secrets' in imported.compose)) compiled.servers[server_name].compose['secrets'] = {};
+								compiled.servers[server_name].compose.secrets[compose_secret_name] = { file: secret_relative };
+
+								secrets.push(compose_secret_name);
+
+								return resolve(compose_secret_name);
+							});
+						});
+
+						await createSecret();
+					}
+
+					if (secrets.length) {
+						compiled.servers[server_name].compose.services[service_name].secrets = secrets;
+					}
+				}
+
+				fs.mkdir(path.dirname(compose_file), { recursive: true }, function (err) {
+					if (err) return null;
+	
+					fs.writeFileSync(compose_file, YAML.stringify(compiled.servers[server_name].compose));
+				});
+
+				/*
 				const imported = compiled.servers[server];
 
-				if (imported.file) imported.file = path.join(compiled.output, imported.file);
-				if (imported.secrets) imported.secrets = path.join(compiled.output, imported.secrets);
+				const file = path.join(compiled.output, imported.file);
+				const secrets = path.join(compiled.output, imported.secrets);
 
-				console.log(imported);
+				console.log(file, imported.file);
+				console.log(secrets, imported.secrets);
+
+				for (const service in imported.compose.services) {
+					const secretsDir = imported.secrets.replace(new RegExp('%__service__%', 'g'), service);
+
+					const secrets = imported.compose.services[service].secrets;
+
+					for (const secret in secrets) {
+						const secretsRel = secretsDir;
+						const secretsAbs = path.join(outputAbsDir, secretsDir);
+
+						console.log(path.join(secretsRel, secret), ' ==> ', path.join(secretsAbs, secret));
+						// console.log(`${path.join(compiled.output, secretsDir)} == ${secret} => ${secrets[secret]}`);
+					}
+				}
+				*/
 			}
 
 			/*
